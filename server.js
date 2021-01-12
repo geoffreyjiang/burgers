9065e0e4db6d12aa078048513714e14dd0acff01
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-01-11 21:16:56 -0800

    added to serverjs

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const eHandle = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(routes);
+
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
