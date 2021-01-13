ce95f81f806833f9e7ef75cd45ad9e82b403fcfc
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-01-12 22:31:29 -0800

    edited handlebars for appjs/working on eatbtn

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
-const eatBtn = document.querySelector('#burgerBtn')
+const eatBtn = document.querySelector('#eatBtn')
+
+if(eatBtn) {
+    if(eatBtn){
+        eatBtn.forEach((button) => {
+            button.addEvenetListener('click', (e) => {
+                e.preventDefault();
+                const id = e.target.getAttrivute("burgId");
+                const devBurg = {devoured: true},
+            })
+        })
+    }
+}
