a499dd6b9e5adb19f2928c057fa7f1757a33f1d4
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-01-12 22:38:45 -0800

    added eatbtn func

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,24 @@ if(eatBtn) {
             button.addEvenetListener('click', (e) => {
                 e.preventDefault();
                 const id = e.target.getAttrivute("burgId");
-                const devBurg = {devoured: true},
+                const devBurg = {devoured: true};
+            
+                fetch(`/api/burgers/${id}`, {
+                    method: "PUT",
+                    headers: {
+                        Accept: 'application.json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(devBurg),
+                })
+                .then((res) => {
+                    if (res.ok) {
+                        location.reload('/')
+                    } else {
+                        alert("Could Not Eat Burger!")
+                    }
+                })
             })
         })
     }
-}
+};
