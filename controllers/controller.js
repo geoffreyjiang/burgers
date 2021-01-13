ce0b77537ab1e9451fc8ccdfc80f2fd0cd3dcdb0
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-01-12 21:50:36 -0800

    worked on post

--- controllers/controller.js
+++ controllers/controller.js
@@ -13,3 +13,20 @@ router.post("/api/burgers", (req, res) => {
 		res.json({ id: results.insertId });
 	});
 });
+
+router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+
+    burger.updateOne(
+    {devoured: req.body.devoured},
+        condition, (result) => {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end()
+        }
+    );
+});
+
+module.exports = router;
+
