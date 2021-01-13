0aa2aff640d246efcb68aaeee0f33e3c620a98fb
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-01-12 21:21:24 -0800

    ok paths work this time

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const burger = require("/model/burger.js")
+const burger = require("../model/burger.js")
+
+router.get('/', (req,res) => {
+    burger.selectAll((data) => {
+        res.render('index', {burgers: data})
+    })
+});
+
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const eHandle = require('express-handlebars');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
