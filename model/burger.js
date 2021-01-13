3a07e8aa5eaaa2a1b500a8f76ab787def5a7a069
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-01-12 18:23:45 -0800

    added functions to js

--- model/burger.js
+++ model/burger.js
@@ -1,1 +1,14 @@
 const orm = require("config/orm.js")
+
+
+const burger = {
+    selectAll(cb) {
+        orm.selectAll("burgers", (res) => cb(res))
+    },
+    insertOne(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, (res) => cb(res));
+    },
+    updateOne(cols, vals, condition, cb) {
+        orm.updateOne("burgers", cols, vals, condition, (res) => cb(res));
+    },
+}
