013c556dc3c92c9bacc601fe4d3ca4f586ac91bf
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-01-14 20:39:32 -0800

    spelling

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if (process.env.JAWSDB_URL) {
   connection.mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection.mysql.createConnection({
-    host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "rjexa5ywikr48627",
     port: 3306,
     password: "ohwwwmf9t9dsvyyj",
