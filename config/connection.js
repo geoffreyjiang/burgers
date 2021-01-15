0490861a11b1358c73c3ee4d7660047188f02a0b
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-01-14 20:10:46 -0800

    heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,17 @@
 const mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection.mysql.createConnection({
+    host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "mfgmhc6m8xm3c92a",
+    port: 3306,
+    password: "ogsp51j4rnjhdalv",
+    database: "wwdip7bgtzfcseqj",
+  })
+}
+
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
