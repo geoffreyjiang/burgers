0cb01ff08634234ce9571093345bc99ee2a867b7
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-01-14 20:45:00 -0800

    fixed heroku

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-  connection.mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection.mysql.createConnection({
+  connection = mysql.createConnection({
     host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "rjexa5ywikr48627",
     port: 3306,
@@ -11,14 +11,6 @@ if (process.env.JAWSDB_URL) {
     database: "bnhdfo5vdpt2dzj0",
   })
 }
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-})
 
 connection.connect((err) => {
     if (err) {
